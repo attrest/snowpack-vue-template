358d0cf9e18d6af941cdd7a2d88a5b4b2c74b363
attrest <attrestmedia@gmail.com>
2022-05-18 18:17:35 +0900

    typescript -> no typescript 패키지로 변경

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -4,11 +4,7 @@ export default {
     public: {url: '/', static: true},
     src: {url: '/dist'},
   },
-  plugins: [
-    '@snowpack/plugin-vue',
-    '@snowpack/plugin-vue/plugin-tsx-jsx.js',
-    '@snowpack/plugin-dotenv',
-  ],
+  plugins: ['@snowpack/plugin-vue', '@snowpack/plugin-dotenv'],
   routes: [
     /* Enable an SPA Fallback in development: */
     // {"match": "routes", "src": ".*", "dest": "/index.html"},
